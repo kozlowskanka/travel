78b65f044b1f72fc812042ba657daa1bed9435f1
Anka Kozłowska <kozlowskanka@gmail.com>
2020-01-18 11:13:51 +0100

    Showing order options

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -2,10 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import OrderSummary from '../Ordersummary/OrderSummary';
 import {Row, Col} from 'react-flexbox-grid';
+import pricing from '../../../data/pricing.json';
+import OrderOption from '../OrderOption/OrderOption';
+import { setOrderOption } from '../../../redux/orderRedux';
 
 const OrderForm = (props) => (
 
   <Row>
+    {pricing.map(option => (
+      <Col key={option.id} md={4}>
+        <OrderOption {...option}
+          currentValue={props.options[option.id]}
+          setOrderOption={setOrderOption} />
+      </Col>
+    ))}
     <Col xs={12}>
       <OrderSummary cost={props.tripCost} options={props.options}/>
     </Col>
